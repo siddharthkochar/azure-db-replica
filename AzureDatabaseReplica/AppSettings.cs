5f3086ced053ae41168540d4cca1511bde5a5981
sidkcr <sidkcr@gmail.com>
2019-05-01 12:56:29 +0530

    initial-commit

---
+++ AzureDatabaseReplica/AppSettings.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+
+namespace AzureDatabaseReplica
+{
+    class AppSettings
+    {
+        public string SqlPackage { get; set; }
+        public Source Source { get; set; }
+        public Destination Destination { get; set; }
+    }
+
+    class ServerCredentials
+    {
+        public string Server { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+
+    class Source : ServerCredentials
+    {
+        public string Database { get; set; }
+        public string Bacpac { get; set; }
+    }
+
+    class Destination : ServerCredentials
+    {
+        public string Database { get; set; }
+        public IReadOnlyCollection<string> Replicas { get; set; }
+    }
+}
---
+++ AzureDatabaseReplica/Execute.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Extensions.Options;
+
+namespace AzureDatabaseReplica
+{
+    class Execute
+    {
+        public Execute(IDatabase database, IOptions<AppSettings> options)
+        {
+            database.Export();
+            database.Import();
+            database.Rename();
+            database.Backup();
+
+            var replicas = options.Value.Destination.Replicas;
+            foreach (var replica in replicas)
+            {
+                database.Restore(replica);
+            }
+        }
+    }
+}
